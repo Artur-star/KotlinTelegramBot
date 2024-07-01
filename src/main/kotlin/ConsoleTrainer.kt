8868964ea6e91511ddf74a4a9a9d7840327a1548
Artut <ArtSoull@yandex.ru>
2024-07-01 20:20:20 +0500

    KTB-05 Develop a simulator menu

--- src/main/kotlin/ConsoleTrainer.kt
+++ src/main/kotlin/ConsoleTrainer.kt
@@ -7,6 +7,26 @@ data class Word(
 )
 
 fun main() {
+    println(
+        "Меню: \n" +
+                "1 – Учить слова\n" +
+                "2 – Статистика\n" +
+                "0 – Выход"
+    )
+    println("Выберите пункт меню: ")
+    var input = readln().toIntOrNull()
+    while (input in 0..2) {
+        when (input) {
+            0 -> println("Нажали 0")
+
+            1 -> println("Нажали 1")
+
+            2 -> println("Нажали 2")
+
+            else -> println("Ошибка ввода данных")
+        }
+        input = readln().toIntOrNull()
+    }
     val dictionary: MutableList<Word> = mutableListOf()
     val file = File("dictionary.txt")
     file.writeText("hello|привет|1\ndog|собака|2\ncat|кошка|3\n")
