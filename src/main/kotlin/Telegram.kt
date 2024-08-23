cbf3fcaf147320979f0a01829e8f2254c8228268
Artur <ArtSoull@yandex.ru>
2024-08-23 23:58:31 +0500

    KTB-16 intermediate change

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -2,23 +2,37 @@ fun main(args: Array<String>) {
     val tbs = TelegramBotService()
     val botToken = args[0]
     var updateId = 0
-    var chatId = 0
+
+
 
     while (true) {
         Thread.sleep(2000)
         val updates = tbs.getUpdates(botToken, updateId)
+        println(updates)
 
         val regexFindUpdateId = "\"update_id\":(.+?),\n\"message\"".toRegex()
         val regexFindText = "\"text\":\"(.+?)\"}".toRegex()
         val regexFindChatId = "\"chat\":\\{\"id\":(.+?),\"first_name\"".toRegex()
+        val regexFindData = "\"data\":\"(.+?)\"".toRegex()
 
         val updateIdString = regexFindUpdateId.find(updates)?.groups?.get(1)?.value ?: continue
-        val text = regexFindText.find(updates)?.groups?.get(1)?.value ?: continue
-        val chatIdString = regexFindChatId.find(updates)?.groups?.get(1)?.value ?: continue
+
+        val text = regexFindText.find(updates)?.groups?.get(1)?.value
+        val chatIdString = regexFindChatId.find(updates)?.groups?.get(1)?.value
+        val data = regexFindData.find(updates)?.groups?.get(1)?.value
 
         updateId = updateIdString.toInt() + 1
-        chatId = chatIdString.toInt()
-        tbs.sendMessage(botToken, chatId, text)
+        val chatId = chatIdString?.toInt()
+
+        if (text?.lowercase() == "hello" && chatId != null) {
+            tbs.sendMessage(botToken, chatId, "Hello")
+        }
+        if (text?.lowercase() == "menu" && chatId != null) {
+            tbs.sendMenu(botToken, chatId)
+        }
+        if (data?.lowercase() == "statistics_clicked" && chatId != null) {
+            tbs.sendMessage(botToken, chatId, "Learned 10 words")
+        }
     }
 }
 
