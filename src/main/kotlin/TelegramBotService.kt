008183a540e24c0a1c530a70972712cc6ac03b4c
Artut <ArtSoull@yandex.ru>
2024-08-23 08:29:59 +0500

    KTB-15 put host in constant

--- src/main/kotlin/TelegramBotService.kt
+++ src/main/kotlin/TelegramBotService.kt
@@ -5,9 +5,11 @@ import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 import java.nio.charset.StandardCharsets
 
+const val TELEGRAM_BASE_URL = "https://api.telegram.org"
+
 class TelegramBotService {
     fun getUpdates(botToken: String, updateId: Int): String {
-        val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
+        val urlGetUpdates = "$TELEGRAM_BASE_URL/bot$botToken/getUpdates?offset=$updateId"
         val client: HttpClient = HttpClient.newHttpClient()
         val httpRequest: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
         val response: HttpResponse<String> = client.send(httpRequest, HttpResponse.BodyHandlers.ofString())
@@ -16,7 +18,7 @@ class TelegramBotService {
 
     fun sendMessage(botToken: String, chatId: Int, text: String) {
         val encodedText = URLEncoder.encode(text, StandardCharsets.UTF_8.toString())
-        val urlSendMessage = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=$encodedText"
+        val urlSendMessage = "$TELEGRAM_BASE_URL/bot$botToken/sendMessage?chat_id=$chatId&text=$encodedText"
         val client: HttpClient = HttpClient.newHttpClient()
         val httpRequest: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlSendMessage)).build()
         client.send(httpRequest, HttpResponse.BodyHandlers.ofString())
